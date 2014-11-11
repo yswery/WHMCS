637f89fc1373dd8c96ebc6cd56d36a138130d715
krishenriksen <krishenriksen@users.noreply.github.com>
2014-11-11 17:21:07 +0100

    Redirect if customer is not on viewinvoice

--- upload/modules/gateways/coinify.php
+++ upload/modules/gateways/coinify.php
@@ -2,9 +2,9 @@
 	function coinify_config()
 	{
 		$configarray = array(
-			"FriendlyName" => array("Type" => "System", "Value" => "coinify"),
+			"FriendlyName" => array("Type" => "System", "Value" => "Coinify"),
 			"api" => array("FriendlyName" => "Invoice API key", "Type" => "text", "Size" => "35", ),
-			"secret" => array("FriendlyName" => "Coinify Secret", "Type" => "text", "Size" => "20", )
+			"secret" => array("FriendlyName" => "IPN Secret", "Type" => "text", "Size" => "20", )
 		);
 		return $configarray;
 	}
@@ -15,14 +15,19 @@
 		curl_setopt_array($ch, array(
 		CURLOPT_URL => 'https://coinify.com/api/v1/invoice',
 		CURLOPT_USERPWD => $params["api"],
-		CURLOPT_POSTFIELDS => 'price=' . number_format($params["amount"], 2, '.', '') . '&currency=' . $params['currency'] . '&item=' . $params["description"] . '&custom=' . json_encode(array('invoiceid' => $params['invoiceid'], 'returnurl' => rawurlencode($params['systemurl']), 'cancelurl' => rawurlencode($params['systemurl']))),
+		CURLOPT_POSTFIELDS => 'price=' . number_format($params["amount"], 2, '.', '') . '&currency=' . $params['currency'] . '&item=' . $params["description"] . '&custom=' . json_encode(array('invoiceid' => $params['invoiceid'], 'returnurl' => rawurlencode($params['systemurl']), 'cancelurl' => rawurlencode($params['systemurl']), 'plugin' => 'WHMCS')),
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_HTTPAUTH => CURLAUTH_BASIC));
 		$url = curl_exec($ch);
 		curl_close($ch);
 
-		$link = '<a href="' . $url . '" title="Coinify Invoice">Pay with Bitcoin</a>';
+		$link = '<a href="' . $url . '">' . $params['langpaynow'] . '</a>';
 
+		if (stripos($params['returnurl'], 'viewinvoice') === false)
+		{
+			header('Location: ' . $url);
+			exit;
+		}
 
 		return $link;
 	}
