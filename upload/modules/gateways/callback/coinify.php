9b4749cb1eba598398446bd9e4c4b757637bad33
Kris Henriksen <krishenriksen@users.noreply.github.com>
2015-08-11 11:07:39 +0200

    Updated to version 3 of Coinify API

--- upload/modules/gateways/callback/coinify.php
+++ upload/modules/gateways/callback/coinify.php
@@ -11,20 +11,33 @@ $gatewaymodule = "coinify"; # Enter your gateway module name here replacing temp
 $gateway = getGatewayVariables($gatewaymodule);
 if (!$gateway["type"]) die("Module Not Activated"); # Checks gateway module is active before accepting callback
 
+// Get the raw HTTP POST body (JSON object encoded as a string)
+// Note: Substitute getBody() with a function call to retrieve the raw HTTP body.
+// In "plain" PHP this can be done with file_get_contents('php://input')
+$body = file_get_contents('php://input');
 
-$hash = hash('sha512', $_POST['transaction']['hash'] . $gateway["secret"]);
+// Get the signature from the HTTP or email headers
+$signature = $_SERVER['HTTP_X_COINIFY_CALLBACK_SIGNATURE'];
+
+// Calculate the signature using the callback data and your IPN secret
+$expected_signature = strtolower(hash_hmac('sha256', $body, $gateway["ipn"], false));
+
+$arr = json_decode($body, true);
+
+// Check that the signatures match
+if (strtolower($signature) != $expected_signature) {
+    // Invalid signature, disregard this callback
+    logTransaction($gateway["name"], $body, 'Unsuccessful'); # Save to Gateway Log: name, data array, status
+    return false;
+}
 
 header('HTTP/1.1 200 OK');
 print '1';
 
-if ($_POST['hash'] == $hash && $_POST['status'] == 1)
-{
-	$invoiceid = checkCbInvoiceID($_POST["custom"]["invoiceid"], $gateway["name"]); # Checks invoice ID is a valid invoice number or ends processing
-	
-	addInvoicePayment($invoiceid, $_POST["transaction"]["hash"], $_POST["fiat"]["amount"], 0, $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
-	logTransaction($gateway["name"], $_POST, 'Successful'); # Save to Gateway Log: name, data array, status
-}
-else
-{
-	logTransaction($gateway["name"], $_POST, 'Unsuccessful'); # Save to Gateway Log: name, data array, status
-}
+$invoiceid = checkCbInvoiceID($arr['data']["custom"]["invoiceid"], $gateway["name"]); # Checks invoice ID is a valid invoice number or ends processing
+
+addInvoicePayment($invoiceid, $arr['data']["bitcoin"]["address"], $arr['data']["native"]["amount"], 0, $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+logTransaction($gateway["name"], $body, 'Successful'); # Save to Gateway Log: name, data array, status
+
+// Valid signature
+return true;
