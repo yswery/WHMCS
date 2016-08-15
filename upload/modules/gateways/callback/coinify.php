720f1f2f5f522427b8c8f8802ba9dbe2ce6b11c8
Morten Berg Nissen <mbn@coinify.com>
2016-08-15 13:47:03 +0200

    COIN-1640 Add comments to why amount is set to an empty string

--- upload/modules/gateways/callback/coinify.php
+++ upload/modules/gateways/callback/coinify.php
@@ -45,14 +45,17 @@ $invoiceid = checkCbInvoiceID($arr['data']["custom"]["invoiceid"], $gateway["nam
 // Get bitcoin address used for payment, as to be used for transaction id
 $txid = $arr['data']["bitcoin"]["address"];
 
-// Blank amount will be the full amount of the invoice
+// Blank amount will be the full amount of the invoice.
+// If 'Convert to for processing' is set, the callback will be in the processing currency and this might
+// not be the same as the invoice currency and we can only add an amount and not currency in addInvoicePayment().
+// So if the invoice is in state 'complete' we assume it is fully paid.
 $amount = '';
 
 checkCbTransID($txid);
 
 switch ($arr['data']['state']) {
     case 'complete': {
-        addInvoicePayment($invoiceid, $txid, $arr['data']["native"]["amount"], 0, $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+        addInvoicePayment($invoiceid, $txid, $amount, 0, $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
         logTransaction($gateway["name"], $body, 'Successful'); # Save to Gateway Log: name, data array, status
         break;
     }
